7b784fa5d3650df922a4368c7ac707290b854de1
Pablo Morelli <pabmorelli@gmail.com>
2023-04-17 17:13:52 +0200

    shorten name

--- tests/dead_letter_receives_event_test.go
+++ tests/dead_letter_receives_event_test.go
@@ -13,7 +13,7 @@ import (
 func TestDeadLetterReceivesEvent(t *testing.T) {
 	// Setup
 	queueName := fmt.Sprintf("queue-%d", time.Now().Unix())
-	deadLetterQueueName := fmt.Sprintf("dead-letter-queue-%d", time.Now().Unix())
+	deadLetterQueueName := fmt.Sprintf("dead-%d", time.Now().Unix())
 	exchangeName := fmt.Sprintf("exchange-%d", time.Now().Unix())
 	routingKey := "order.orderCreated"
 
