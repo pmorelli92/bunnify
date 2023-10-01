11f9dd12db16a664ef3be039757081a19beaca80
Pablo Morelli <pabmorelli@gmail.com>
2023-10-01 20:57:29 +0200

    split further into different files

--- bunnify/consumer.go
+++ bunnify/consumer.go
@@ -87,52 +87,6 @@ func (c Consumer) Consume() error {
 	return nil
 }
 
-func getDeliveryInfo(queueName string, delivery amqp.Delivery) DeliveryInfo {
-	deliveryInfo := DeliveryInfo{
-		Queue:      queueName,
-		Exchange:   delivery.Exchange,
-		RoutingKey: delivery.RoutingKey,
-	}
-
-	// If routing key is empty, it is mostly due to the event being dead lettered.
-	// Check for the original delivery information in the headers
-	if delivery.RoutingKey == "" {
-		deaths, ok := delivery.Headers["x-death"].([]interface{})
-		if !ok || len(deaths) == 0 {
-			return deliveryInfo
-		}
-
-		death, ok := deaths[0].(amqp.Table)
-		if !ok {
-			return deliveryInfo
-		}
-
-		queue, ok := death["queue"].(string)
-		if !ok {
-			return deliveryInfo
-		}
-		deliveryInfo.Queue = queue
-
-		exchange, ok := death["exchange"].(string)
-		if !ok {
-			return deliveryInfo
-		}
-		deliveryInfo.Exchange = exchange
-
-		routingKeys, ok := death["routing-keys"].([]interface{})
-		if !ok || len(routingKeys) == 0 {
-			return deliveryInfo
-		}
-		key, ok := routingKeys[0].(string)
-		if !ok {
-			return deliveryInfo
-		}
-		deliveryInfo.RoutingKey = key
-	}
-
-	return deliveryInfo
-}
-
 func (c Consumer) createExchanges(channel *amqp.Channel) error {
 	errs := make([]error, 0)
 
